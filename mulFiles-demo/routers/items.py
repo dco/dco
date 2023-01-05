8a010abf0251120443ffcd7c1f58e752521bb533
dco <uiqian@live.cn>
2023-01-05 22:33:34 +0800

    update router

--- mulFiles-demo/routers/items.py
+++ mulFiles-demo/routers/items.py
@@ -26,7 +26,6 @@ async def read_item(item_id: str):
         raise HTTPException(status_code=404, detail="Item not found")
     return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
 
-
 @router.put(
     "/{item_id}",
     tags=["custom"],
