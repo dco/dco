43a2d7d2bb7a03abb62f67b19ce0fc3f3702c9e2
dco <uiqian@live.cn>
2023-01-06 00:07:34 +0800

    index root

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+
+import sys
+sys.path.append("./packages")
+
 import uvicorn
 from fastapi import FastAPI
+import config 
+import index
+from fastapi.responses import HTMLResponse
+
+
+conf = config.Settings()
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    return index.index_html
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info",reload=True)
