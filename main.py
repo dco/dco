6b04b4d59a65e6cdf06528f1d9658661b1d1140e
dco <uiqian@live.cn>
2023-01-12 17:02:09 +0800

    预设值

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from enum import Enum
+
 
 #自定义包
 from build.dco import config 
@@ -15,16 +17,36 @@ conf = config.Settings()
 print(conf.app_name)
 index = Jinja2Templates("docs")
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 app = FastAPI()
+
 app.mount("/docs",StaticFiles(directory='docs'),name="docs")
 
+# 路径操作装饰器，就是路由
 @app.get("/")
+# 路径操作函数
 async def root(request: Request):
     return index.TemplateResponse("index.html",{"request": request})
 
+# 使用路径参数
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
+# 预设值
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 
 
